632782b002e865d4775ac24dadcd843c0a338869
Hiep <doandaihiep2021@gmail.com>
2024-09-04 00:26:15 +0700

    update trpc route handler

--- src/app/api/trpc/[trpc]/route.ts
+++ src/app/api/trpc/[trpc]/route.ts
@@ -11,4 +11,10 @@ const handler = (request: Request) => {
   });
 };
 
-export { handler as GET, handler as POST };
+export async function GET(request: Request) {
+  return handler(request);
+}
+
+export async function POST(request: Request) {
+  return handler(request);
+}
